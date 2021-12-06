8e2118be7bf39435d4d67f55c703f8c28105741e
YadanarLin <s1F102100030@iniad.org>
2021-12-06 07:17:12 +0630

    more changes

--- line_bot/line_message.py
+++ line_bot/line_message.py
@@ -3,7 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 import urllib.request
 import json
 
-REPLY_ENDPOINT_URL='https://api.line.me'
+REPLY_ENDPOINT_URL='https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN='fVxDRaj5HyhguClw4ZkbjWvGLn2uR216HpNYCad7o8KGZ7erMxXODrZex1gd9NzrywWxcw5HdGx538f1z2wWcDb6kYAGBMPcMi8E19tBHr2K0cqLsfRNNTEyoZu5GPLUnA9At0/69RsISPjv9vh7WwdB04t89/1O/w1cDnyilFU='
 HEADER={
     'Content-Type' : 'application/json',
