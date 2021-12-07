c45f3e4aa87c9e707b574d53856a5c865eb40b87
YadanarLin <s1F102100030@iniad.org>
2021-12-07 13:52:56 +0630

    change url

--- config/urls.py
+++ config/urls.py
@@ -23,8 +23,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     #call index function in the apiTest_views
-    path('line_bot/',include('line_bot.urls')),
+    path('',include('line_bot.urls')),
     url(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
 
